39e1352210de1cb06609caea3bea585acadaa69f
wxke <wangxkwang@outlook.com>
2019-02-12 21:59:41 +0800

    commit

--- 排序/sort—text.py
+++ 排序/sort—text.py
@@ -12,6 +12,7 @@ def select_sort(data):
             if data[j] <data[tmp]:
                 tmp = j
         data[i],data[tmp] = data[tmp],data[i]
+
 def insert_sort(data):
     for i in range(1,len(data)):
         tmp = data[i]
@@ -23,6 +24,7 @@ def insert_sort(data):
             else:
                 break
         data[j+1] = tmp
+
 def quick_sort(data,left,right):
     l = left
     r = right
@@ -39,8 +41,71 @@ def quick_sort(data,left,right):
         quick_sort(data,mid+1,right)
         quick_sort(data,left,mid-1)
 
-data = list(range(10000))
+def make_dui_sort(data,low,high):
+    i = low
+    j = 2*i+1
+    tmp = data[i]
+    while j <= high:
+        if j < high and data[j+1] > data[j]:
+                j = j+1
+        if tmp < data[j]:
+            data[i] = data[j]
+            i = j
+            j = 2*i+1
+        else:
+            break
+    data[i] = tmp
 
+def dui_sort(data):
+    n = len(data)
+    for i in range(n//2-1,-1,-1):
+        make_dui_sort(data,i, n-1)
+    for i in range(n):
+        data[n-1-i],data[0] = data[0],data[n-1-i]
+        make_dui_sort(data,0,n-i-2)
+
+def merge(data,low,mid,high):
+    i = low
+    j = mid + 1
+    li = []
+    while i <= mid and j <= high:
+        if data[i] <= data[j]:
+            li.append(data[i])
+            i += 1
+        else:
+            li.append(data[j])
+            j += 1
+    while i <= mid:
+        li.append(data[i])
+        i += 1
+    while j <= high:
+        li.append(data[j])
+        j += 1
+    data[low:high+1] = li
+
+def mergesort(data,low,high):
+    if low < high:
+        mid = (low + high) // 2
+        mergesort(data,low,mid)
+        mergesort(data,mid+1,high)
+        merge(data,low,mid,high)
+
+def use_mergesort(data):
+    mergesort(data,0,len(data)-1)
+
+
+def shell_sort(data):
+    flag = len(data) // 2
+    while flag:
+        for i in range(flag,len(data)):
+            tmp = data[i]
+            j = i - flag
+            while j>=0 and data[j] > tmp:
+                data[j + flag] = data[j]
+                j = j - flag
+            data[j + flag] = tmp
+        flag //= 2
+data = list(range(10))
 random.shuffle(data)
-quick_sort(data,0,len(data)-1)
+shell_sort(data)
 print(data)
